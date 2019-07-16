1ec6b470cc827e7693f8b1a44dda1c3002beaecf
Kirill <thegaldren@gmail.com>
2019-07-16 11:32:31 +0300

    add Interaction class

---
+++ src/main/java/com/trulden/Interaction.java
@@ -0,0 +1,52 @@
+package com.trulden;
+
+import java.util.Date;
+import java.util.HashSet;
+
+public class Interaction {
+    private int id;
+    private Date date;
+    private String type;
+    private HashSet<String> personNames;
+    private String comment;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public HashSet<String> getPersonNames() {
+        return personNames;
+    }
+
+    public void setPersonNames(HashSet<String> personNames) {
+        this.personNames = personNames;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+}
