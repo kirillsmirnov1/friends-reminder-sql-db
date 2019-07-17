040afeca5754ab696529e50eabd4a177d2933ff7
Kirill <thegaldren@gmail.com>
2019-07-17 12:12:01 +0300

    change date format to correspond SQLite standart

--- src/main/java/com/trulden/Util.java
+++ src/main/java/com/trulden/Util.java
@@ -3,5 +3,5 @@ package com.trulden;
 import java.text.SimpleDateFormat;
 
 public class Util {
-    public static final SimpleDateFormat dateFormat = new SimpleDateFormat("dd MM yyyy");
+    public static final SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
 }
