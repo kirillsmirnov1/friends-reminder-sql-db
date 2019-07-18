2c3e15c532ed5710aaded3e185f25ce63d7e8d8d
Kirill <thegaldren@gmail.com>
2019-07-18 17:17:28 +0300

    add daysPassed()

--- src/main/java/com/trulden/Util.java
+++ src/main/java/com/trulden/Util.java
@@ -1,7 +1,13 @@
 package com.trulden;
 
 import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
 public class Util {
     public static final SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+
+    public static int daysPassed(Date date) {
+        return (int) TimeUnit.DAYS.convert(new Date().getTime() - date.getTime(), TimeUnit.MILLISECONDS);
+    }
 }
